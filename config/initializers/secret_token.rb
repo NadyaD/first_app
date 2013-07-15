e4555bafd44239f58a82e3955c1ff227f74a82dc
Nadya Denissova <nadya.denissova@digiflare.com>
2013-07-15 15:56:21 -0400

    adding first_app files for the first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '86b734787ac073805576a54014ee35f4312e48771f67b1083ddc51679a6bcfa8b99e958ba7350f90effe2caa2a87d6e9c6f7a79e94c247e53019ef2137f668b5'
